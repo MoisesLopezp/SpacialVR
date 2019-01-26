0b48f893dbeb12ef59f7b3d9e1c8cac738dc1b1c
PacoFMD <pacomonfmd@hotmail.com>
2019-01-25 22:51:51 -0600

    Carpetas Paco

---
+++ Assets/Paco/Scripts/ItemsPeligro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsPeligro : MonoBehaviour {
+
+    public int damage = 0;
+    public int tipoObj;
+    StatsPlayer PlayerScript;
+
+    
+	// Use this for initialization
+	void Start () {
+        PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<StatsPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerScript.JugadorItem(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Paco/Scripts/StatsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsPlayer : MonoBehaviour {
+    public int Salud = 100, SaludTotal;
+	// Use this for initialization
+	void Start () {
+		SaludTotal = Salud;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void JugadorItem(int _stat)
+    {
+        SaludTotal += _stat;
+    }
+}
