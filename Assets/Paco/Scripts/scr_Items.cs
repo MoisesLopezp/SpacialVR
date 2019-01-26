1dd7ad791e83e301a805b1d664f7901985540b5c
PacoFMD <pacomonfmd@hotmail.com>
2019-01-26 01:18:18 -0600

    Items integrado
     en un solo script intereactuando con Stats

--- Assets/Paco/Scripts/scr_Items.cs
+++ Assets/Paco/Scripts/scr_Items.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemsPeligro : MonoBehaviour {
+public class scr_Items : MonoBehaviour {
     /*
     TipoObj
     --Peligro
@@ -19,25 +19,49 @@ public class ItemsPeligro : MonoBehaviour {
     11 = comunicadores
     */
 
+    public float fuerzaini = 1.0f;
     public float damage = 0;
     float magnitud;
 
+
     public int tipoObj;
-    
-    public bool detonante = false;
+    public int tipoobjeto, dmg = 10;
+
+    public bool detonante = false; // para que explote y haga daño se necesita cambiar el bool a true
     bool multiplicador = false;
-      
-    RaycastHit hit;  
+    bool hoyonegro = false;
+
+    RaycastHit hit;
+    Transform Jugador;
     GameObject player;
     Vector3 miPosCentro;
+    Rigidbody mirigi, jugarigi;
     scr_PlayerStats PlayerScript;
 
     // Use this for initialization
     void Start () {
         PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_PlayerStats>();
         player = GameObject.FindGameObjectWithTag("Player");
-        
-	}
+
+        mirigi = GetComponent<Rigidbody>();
+        Jugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        switch (tipoobjeto)
+        {
+            case 1://es meteoro
+                Vector3 Direccion = (Jugador.transform.position - this.transform.position).normalized;
+                Direccion = new Vector3(Direccion.x + Random.Range(-0.1f, 0.1f), Direccion.y + Random.Range(-0.1f, 0.1f), Direccion.z + Random.Range(-0.1f, 0.1f));
+                mirigi.AddForce(Direccion * fuerzaini, ForceMode.Impulse);
+                break;
+            case 2://hoyonegro
+                hoyonegro = !hoyonegro;
+                jugarigi = Jugador.GetComponentInChildren<Rigidbody>();
+                break;
+            default:
+                break;
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -55,14 +79,12 @@ public class ItemsPeligro : MonoBehaviour {
         {
             switch (tipoObj)
             {
-                case 1:
-                    break;
+                
                 case 2:
                     PlayerScript.Add_Dmg(damage);
                     Destroy(this.gameObject);
                     break;
-                case 3:
-                    break;
+               
                 case 4:
                     damage -= magnitud;
                    
@@ -77,10 +99,25 @@ public class ItemsPeligro : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "Player" && tipoObj == 3)
+        if(other.gameObject.tag == "Player" )
         {
-            PlayerScript.Add_Dmg(damage);
-            //Debug.Log(PlayerScript.SaludTotal);
+            switch (tipoObj)
+            {
+                case 3:
+                    PlayerScript.Add_Dmg(damage);                    
+                    break;
+                case 5:
+                    Vector3 nuevadireccion = (this.transform.position - Jugador.transform.position).normalized;
+                    float distance = Vector3.Distance(this.transform.position, Jugador.transform.position);
+                    if (distance < 0.2f) { distance = 0.2f; }
+                    float force = fuerzaini / (distance * distance);
+                    jugarigi.AddForce(nuevadireccion * force);
+                    break;                
+                default:
+                    break;
+            }
+
+            
         }
     }
 
@@ -134,8 +171,7 @@ public class ItemsPeligro : MonoBehaviour {
             if (hit.collider.gameObject.tag == "Player")
             {
                 Vector3 dirActual = (this.transform.position - player.transform.position);
-                magnitud = dirActual.magnitude;
-                
+                magnitud = dirActual.magnitude;            
 
                
                 //Debug.DrawLine(transform.position, transform.forward, Color.red); print("Hit");
