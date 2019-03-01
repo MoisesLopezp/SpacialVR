2bd8b5e98be1393ab53cc1905a0f1f5e16e8bc30
AlainBarragan <barraganalain55@gmail.com>
2019-03-01 11:53:35 -0600

    ahora en el 2018

---
+++ Assets/Alain's Things/Scripts/SCR_Controller.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Controller : MonoBehaviour
+{
+    private Valve.VR.EVRButtonId vr_Trigger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+}
